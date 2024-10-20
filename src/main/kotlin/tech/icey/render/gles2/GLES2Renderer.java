f2a9af8605c481726290b97cd86539a64e9c62fb
Chuigda WhiteGive <icey@icey.tech>
2024-10-21 02:46:44 +0800

    I wish I could make her grin, all she wants are things back the way they've been.

---
+++ src/main/kotlin/tech/icey/render/gles2/GLES2Renderer.java
@@ -0,0 +1,5 @@
+package tech.icey.render.gles2;
+
+import tech.icey.render.IRenderer;
+
+public final class GLES2Renderer implements IRenderer {}
---
+++ src/main/kotlin/tech/icey/render/vk/VulkanRenderer.java
@@ -0,0 +1,5 @@
+package tech.icey.render.vk;
+
+import tech.icey.render.IRenderer;
+
+public final class VulkanRenderer implements IRenderer {}
